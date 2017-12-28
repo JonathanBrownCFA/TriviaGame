4599308b202fc13aa794b1f0fd269c8c41a7b528
JonathanBrownCFA <jbrown1@gmail.com>
2017-12-28 09:51:27 -0500

    beautify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,87 @@
 /*jslint browser: true*/ /*global  $*/
 
-$(document).ready(function () {
-    $('#reset').click(function () {
-        location.reload();
-    });
-    $("#timer").click(function () {
-        var count = 30;
+$(document).ready(function() {
+  $("#reset").click(function() {
+    location.reload();
+  });
+  $("#timer").click(function() {
+    var count = 30;
 
+    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
-        var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+    function timer() {
+      count = count - 1;
+      if (count <= -1) {
+        clearInterval(counter);
+        document.getElementById("countdown").innerHTML = "Game Over";
+        alert("Time is up!");
+      } else {
+        document.getElementById("countdown").innerHTML = count + "";
+      }
 
-        function timer() {
-            count = count - 1;
-            if (count <= -1) {
+      $(document).ready(function() {
+        $("#results").click(function() {
+          {
+            var cat1name = "1";
+            var cat2name = "2";
+            var cat3name = "3";
+            var cat4name = "4";
+            var cat11name = "None";
 
-                clearInterval(counter);
-                document.getElementById("countdown").innerHTML = "Game Over";
-                alert("Time is up!");
+            var cat1 = $("input[@name=q1]:checked").val() != "a";
+            var cat2 = $("input[@name=q2]:checked").val() != "b";
+            var cat3 = $("input[@name=q3]:checked").val() != "b";
+            var cat4 = $("input[@name=q4]:checked").val() != "b";
 
-            } else {
-                document.getElementById("countdown").innerHTML = count + "";
+            var cat11 = !cat1 && !cat2 && !cat3 && !cat4;
+            var categories = [];
+
+            if (cat1) {
+              categories.push(cat1name);
+            }
+            if (cat2) {
+              categories.push(cat2name);
+            }
+            if (cat3) {
+              categories.push(cat3name);
+            }
+            if (cat4) {
+              categories.push(cat4name);
+            }
+            if (cat11) {
+              categories.push(cat11name);
             }
 
-            $(document).ready(function () {
-                $("#results").click(function () {
+            var catStr =
+              "You answered the following questions incorrectly: " +
+              categories.join(", ") +
+              "";
+            $("#categorylist").html(catStr);
+            $("#categorylist").show("slow");
 
-                    {
-                        var cat1name = "1";
-                        var cat2name = "2";
-                        var cat3name = "3";
-                        var cat4name = "4";
-                        var cat11name = "None";
-
-
-                        var cat1 = ($("input[@name=q1]:checked").val() != "a");
-                        var cat2 = ($("input[@name=q2]:checked").val() != "b");
-                        var cat3 = ($("input[@name=q3]:checked").val() != "b");
-                        var cat4 = ($("input[@name=q4]:checked").val() != "b");
-
-
-
-                        var cat11 = (!cat1 && !cat2 && !cat3 && !cat4);
-                        var categories = [];
-
-                        if (cat1) {
-                            categories.push(cat1name)
-                        };
-                        if (cat2) {
-                            categories.push(cat2name)
-                        };
-                        if (cat3) {
-                            categories.push(cat3name)
-                        };
-                        if (cat4) {
-                            categories.push(cat4name)
-                        };
-                        if (cat11) {
-                            categories.push(cat11name)
-                        };
-
-
-
-                        var catStr = 'You answered the following questions incorrectly: ' + categories.join(', ') + '';
-                        $("#categorylist").html(catStr);
-                        $("#categorylist").show("slow");
-
-                        if (cat1) {
-                            $("#category1").text();
-                            $("#category1").show("slow");
-                        }
-                        if (cat2) {
-                            $("#category1").text();
-                            $("#category2").show("slow");
-                        }
-                        if (cat3) {
-                            $("#category1").text();
-                            $("#category3").show("slow");
-                        }
-                        if (cat4) {
-                            $("#category1").text();
-                            $("#category4").show("slow");
-                        }
-                        if (cat11) {
-                            $("#category1").text();
-                            $("#category11").show("slow");
-                        }
-                    }
-
-                });
-            });
-        }
-    });
+            if (cat1) {
+              $("#category1").text();
+              $("#category1").show("slow");
+            }
+            if (cat2) {
+              $("#category1").text();
+              $("#category2").show("slow");
+            }
+            if (cat3) {
+              $("#category1").text();
+              $("#category3").show("slow");
+            }
+            if (cat4) {
+              $("#category1").text();
+              $("#category4").show("slow");
+            }
+            if (cat11) {
+              $("#category1").text();
+              $("#category11").show("slow");
+            }
+          }
+        });
+      });
+    }
+  });
 });
